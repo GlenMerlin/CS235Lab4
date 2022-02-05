59207769dc84966c1cb522e753475b46d9ae953f
GlenMerlin <Glenmerlin@protonmail.com>
2022-02-05 15:50:39 -0700

    reading successful

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -1,30 +1,88 @@
-#include <fstream>
 #include "Pathfinder.h"
-#include <string>
-#include <sstream>
 
 using namespace std;
 
 bool Pathfinder::importMaze(string file_name)
-{
-    cout << "importMaze from " << file_name << endl;
-    ifstream file(file_name.c_str());
-    if (file.is_open())
-    {
-        string line;
-        int row_count = 0;
-        for (int row = 0; row < ROW_SIZE; row++)
-        {
-            getline(file, line);
-            stringstream ss(line);
-            for (int col = 0; col < COL_SIZE; col++)
-            {
-                int value;
-                ss >> value;
-                cout << "[" << row << "][" << col << "]=" << value << endl;
-                maze_grid[row][col] = value;
-            }
-        }
+	{
+		cout << "importMaze from "<<file_name<<endl;
+		ifstream file (file_name.c_str());
+		if (file.is_open()) {
+			string line;
+			int row_count = 0;
+      for (int depth = 0; depth < DEPTH_SIZE; depth++){
+        for(int row = 0; row < ROW_SIZE; row++) {
+				getline(file, line);
+				stringstream ss(line);
+			  	for(int col = 0; col < COL_SIZE; col++) {
+				  	int value;
+					  ss >> value;
+					  cout << "["<<row<<"]["<<col<<"]["<<depth<<"]="<<value<<endl;
+					  maze_grid[col][row][depth] = value;
+				  }
+			  }
+      }
+			
+		}
+	    return(true);
+	}
+
+string Pathfinder::toString() const
+	{
+	  stringstream ss;
+    for (int depth = 0; depth < DEPTH_SIZE; depth++){
+      for(int row = 0; row < ROW_SIZE; row++) {
+	      for(int col = 0; col < COL_SIZE; col++) {
+	           ss << maze_grid[row][col][depth];		
+	      }
+	        ss << endl;
+	    }
     }
-    return (true);
-};
+	  return ss.str();
+	}
+
+
+vector<string> Pathfinder::solveMaze() {
+  find_maze_path(maze_grid, 0,0,0);
+	    for(auto s:solution) {
+		cout <<s<<endl;
+	    }
+	    return solution;
+}
+
+void Pathfinder::createRandomMaze() {
+
+}
+
+bool Pathfinder::find_maze_path(int grid[ROW_SIZE][COL_SIZE][DEPTH_SIZE], int r, int c, int d) {
+	  cout << "find_maze_path ["<<r<<"]["<<c<<"]["<<d<<"]"<<endl;
+	  cout << this->toString();
+	  if (r < 0 || c < 0 || d < 0 ||r >= ROW_SIZE || c >= COL_SIZE || d >= DEPTH_SIZE)
+	    return false;      // Cell is out of bounds.
+	  else if (grid[r][c][d] != BACKGROUND)
+	    return false;      // Cell is on barrier or dead end.
+	  else if (r == ROW_SIZE - 1 && c == COL_SIZE - 1 && d == DEPTH_SIZE -1) {
+	    grid[r][c][d] = PATH;         // Cell is on path
+	    solution.push_back("("+to_string(r)+","+to_string(c)+","+to_string(d)+")");
+	    return true;               // and is maze exit.
+	  }
+	  else { 
+	    // Recursive case.
+	    // Attempt to find a path from each neighbor.
+	    // Tentatively mark cell as on path.
+	    grid[r][c][d] = PATH;
+	    if (find_maze_path(grid, r - 1, c, d) // Up
+	        || find_maze_path(grid, r + 1, c, d ) // Down
+	        || find_maze_path(grid, r, c - 1, d ) // Left
+	        || find_maze_path(grid, r, c + 1, d ) // Right
+          || find_maze_path(grid, r, c, d + 1 ) // Forward
+          || find_maze_path(grid, r, c, d - 1) ) { // Back
+	      solution.push_back("("+to_string(r)+","+to_string(c)+","+to_string(d)+")");
+	      return true;
+	    }
+	    else {
+	      grid[r][c][d] = TEMPORARY;  // Dead end.
+	      return false;
+	    }
+	  }
+	  return false;
+	}
--- Pathfinder.h
+++ Pathfinder.h
@@ -1,15 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
 #include "PathfinderInterface.h"
+
 using namespace std;
 
 
-class Pathfinder: protected PathfinderInterface {
-protected:
-  const int ROW_SIZE = 5;
-  const int COL_SIZE = 5;
-  const int BACKGROUND = 1;
-  const int WALL = 0;
-  const int TEMPORARY = 2; // Used to show this path has been explored
-  const int PATH = 3;
-  int maze_grid[5][5]; // To hold values
-  vector<string> solution;
+const int ROW_SIZE = 5;
+const int COL_SIZE = 5;
+const int DEPTH_SIZE = 5;
+class Pathfinder: public PathfinderInterface {
+  protected:
+    
+    const int BACKGROUND = 1;
+    const int WALL = 0;
+    const int TEMPORARY = 2; // Used to show this path has been explored
+    const int PATH = 3;
+    int maze_grid[ROW_SIZE][COL_SIZE][DEPTH_SIZE]; // To hold values
+    vector<string> solution;
+  public:
+    Pathfinder() {
+    
+    }
+    ~Pathfinder() {
+
+    }
+    string toString() const;
+    void createRandomMaze();
+    bool importMaze(string file_name);
+    bool find_maze_path(int grid[ROW_SIZE][COL_SIZE][DEPTH_SIZE], int r, int c, int d);
+    vector<string> solveMaze();
 };
